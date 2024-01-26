dc5e9a9c8d4e743c46846c83e6c7d0ced79b886b
rachell-calhoun <86750138+rachell-calhoun@users.noreply.github.com>
2024-01-25 20:30:06 -0500

    Update utilities.py

--- streamlitapp/utilities.py
+++ streamlitapp/utilities.py
@@ -380,7 +380,7 @@ def get_county_details_url():
     if debug == "true":
         url = "http://localhost:8501/?fips="
     else:
-        url = "https://citizenlabs-upwardmobility-raca.streamlit.app/?fips="
+        url = "https://citizenlabs-upwardmobility-raca.streamlitapp.com/?fips="
 
     return url
 
